a62aa1f6b7b00ba09702e1cdec494089bf09e964
Lewis Bull <lewis@verve-homecare.co.uk>
2025-06-04 10:10:44 +0100

    Added a basic bootstrap navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const NavBar = () => {
-    return <div>navbar placeholder</div>
+       return <Navbar expand="lg" className="bg-body-tertiary">
+        <NavBar.Brand href="#home">React-Bootstrap</NavBar.Brand>
+        <NavBar.Toggle aria-controls="basics-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+
+          </Nav>
+
+        </Navbar.Collapse>
+       </Navbar>
 };
 
 export default NavBar
