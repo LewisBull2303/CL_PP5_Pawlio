dcc796c8d0ffc07acf7788f2c7a01ba256fd45ac
Lewis Bull <lewis@verve-homecare.co.uk>
2025-06-19 12:23:59 +0100

    Updated styles in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styles from '../src/styles/Avatar.module.css';
+import styles from '../src/App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
