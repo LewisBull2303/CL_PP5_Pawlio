95d7dc9e9be86797a86626182fc15055f41a645c
Lewis Bull <lewis@verve-homecare.co.uk>
2025-06-25 11:53:40 +0100

    Updated the axios link to the correct project

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://dripdrop-drf-api-8425aae53a44.herokuapp.com/';
+axios.defaults.baseURL = 'https://cl-pp5-pawlio-ba8f8e344581.herokuapp.com';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
