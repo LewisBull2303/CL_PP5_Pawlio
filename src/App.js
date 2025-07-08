1b72182303cb404d8e9929de80e8c70769b054a3
Lewis Bull <lewis@verve-homecare.co.uk>
2025-07-08 10:24:10 +0100

    Updated the posts create url

--- src/App.js
+++ src/App.js
@@ -53,7 +53,11 @@ function App() {
           />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route
+            exact
+            path="/posts/post/add"
+            render={() => <PostCreateForm />}
+          />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
