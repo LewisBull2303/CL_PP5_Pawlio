22a7b4a280a37b83863410404c088eacb7e3cc0c
Lewis Bull <lewis@verve-homecare.co.uk>
2025-07-10 15:55:19 +0100

    Removed a useless line of code in the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,7 +28,6 @@ const NavBar = () => {
       await axios.post('dj-rest-auth/logout/');
       setCurrentUser(null);
       setExpanded(false);
-      removeTokenTimestamp();
     } catch (err) {
       //console.log(err)
     }
