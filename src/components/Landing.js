37101c4d1b1b7311bc9c30bfad1788615182eb89
Lewis Bull <lewis@verve-homecare.co.uk>
2025-07-10 12:59:02 +0100

    Added a landing page component and added a landing page image

---
+++ src/components/Landing.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Card, Button, Row, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import landingImage from '../assets/landing-page-image.png';
+import styles from '../styles/Landing.module.css';
+import appStyles from '../App.module.css';
