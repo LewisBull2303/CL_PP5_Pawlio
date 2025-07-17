46b3ce7bb09ca8f778f8767ac50964e6d7ec4ce0
Lewis Bull <lewis@verve-homecare.co.uk>
2025-07-17 16:25:52 +0100

    Updated the comment create page

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
-import { Form, Alert } from 'react-bootstrap';
-import InputGroup from 'react-bootstrap/InputGroup';
-
+import { Button, Form, InputGroup } from 'react-bootstrap';
 import styles from '../../styles/CommentCreateEditForm.module.css';
 import Avatar from '../../components/Avatar';
 import { axiosRes } from '../../api/axiosDefaults';
-import appStyles from '../../App.module.css';
 
 function CommentCreateForm(props) {
   const { post, setPost, setComments, profileImage, profile_id } = props;
   const [content, setContent] = useState('');
-  const [errors, setErrors] = useState({});
 
-  const handleChange = (event) => {
-    setContent(event.target.value);
+  /* 
+    Handles changes to the create comment input field
+  */
+  const handleChange = (e) => {
+    setContent(e.target.value);
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  /* 
+    Handles the submission of the comment create input field
+    Increments the number of comments by 1
+  */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
       const { data } = await axiosRes.post('/comments/', {
         content,
@@ -33,52 +35,51 @@ function CommentCreateForm(props) {
         results: [
           {
             ...prevPost.results[0],
-            comments_count: prevPost.results[0].comments_count + 1,
+            comments_number: prevPost.results[0].comments_number + 1,
           },
         ],
       }));
       setContent('');
     } catch (err) {
       // console.log(err);
-      setErrors(err.response?.data);
     }
   };
 
   return (
-    <Form className="mt-2" onSubmit={handleSubmit}>
+    <Form className="mt-2 text-center" onSubmit={handleSubmit}>
       <Form.Group>
         <InputGroup>
-          <Link to={`/profiles/${profile_id}`}>
-            <img
-              src={profileImage}
-              width={55}
-              height={55}
-              alt="avatar"
-              style={{ borderRadius: '50%' }}
-            />
-          </Link>
+          <p className="my-2">
+            <Link to={`/profiles/${profile_id}`}>
+              <img
+                src={profileImage}
+                width={55}
+                height={55}
+                alt="avatar"
+                style={{ borderRadius: '50%' }}
+              />
+            </Link>
+          </p>
           <Form.Control
             className={styles.Form}
-            placeholder="my comment..."
+            placeholder="share what you think..."
+            aria-label="comment input box"
             as="textarea"
             value={content}
             onChange={handleChange}
             rows={2}
           />
         </InputGroup>
-        {errors.content?.map((message, idx) => (
-          <Alert variant="warning" className={appStyles.Alert} key={idx}>
-            {message}
-          </Alert>
-        ))}
       </Form.Group>
-      <button
-        className={`${styles.Button} btn d-block ml-auto`}
-        disabled={!content.trim()}
+
+      <Button
+        className={styles.CommentsButton}
+        onMouseDown={(e) => e.preventDefault()}
         type="submit"
       >
-        post
-      </button>
+        Add
+      </Button>
+      <hr className={styles.Line} />
     </Form>
   );
 }
