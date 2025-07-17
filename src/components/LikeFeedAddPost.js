20ee852b858ca17336af4e74e69eacaae9254ae7
Lewis Bull <lewis@verve-homecare.co.uk>
2025-07-17 10:13:14 +0100

    Added a like icon

--- src/components/LikeFeedAddPost.js
+++ src/components/LikeFeedAddPost.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import appStyles from '../App.module.css';
 import styles from '../styles/SmallMenuContainer.module.css';
+import LikeIcon from '../assets/LikeIcon.png';
 
 const LikeFeedAddPost = () => {
   return (
