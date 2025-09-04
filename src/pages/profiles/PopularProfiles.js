cfaec6bfaa5b4f1276a24fe11e830783d6609e44
Lewis Bull <lewisb@aldingbourne.org>
2025-09-04 10:35:34 +0100

    Added a truncate feature to the popular profiles so it doesnt ruin the mobile screens

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -6,6 +6,13 @@ import Asset from "../../components/Asset";
 import Profile from "./Profile";
 import { useProfileData } from "../../contexts/ProfileDataContext";
 
+
+/*
+  Truncates usernames longer than 6 characters and adds an ellipsis
+*/
+const truncateUsername = (username) =>
+  username.length > 6 ? username.slice(0, 6) + "…" : username;
+
 /*
   Displays first three most followed profiles in the app
 */
@@ -19,8 +26,10 @@ const PopularProfiles = () => {
           <p>Top Users</p>
           <div className={styles.UsersDisplay}>
             {popularProfiles.results.slice(0, 3).map((profile) => (
-              <Profile key={profile.id} profile={profile} />
-              
+              <Profile
+                key={profile.id}
+                profile={{ ...profile, owner: truncateUsername(profile.owner) }}
+              />
             ))}
           </div>
         </>
