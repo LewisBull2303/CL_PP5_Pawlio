592fe30de7ef98e7213fd37e819d277913f91f60
Lewis Bull <lewisb@aldingbourne.org>
2025-09-05 14:25:36 +0100

    Removed styles as the code was no longer working

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { axiosRes } from "../api/axiosDefaults";
-import styles from "../styles/Button.module.css";
 
 const SaveButton = ({ postId, isInitiallySaved }) => {
   const [isSaved, setIsSaved] = useState(isInitiallySaved);
@@ -31,7 +30,7 @@ const SaveButton = ({ postId, isInitiallySaved }) => {
       onClick={isSaved ? handleUnsave : handleSave}
       style={{ cursor: "pointer", marginLeft: "10px" }}
     >
-      {isSaved ? <i class="fa-solid fa-bookmark" className={`${styles.Button}`}></i> : <i class="fa-solid fa-bookmark" className={`${styles.Button}`}></i>}
+      {isSaved ? <i class="fa-solid fa-bookmark"></i> : <i class="fa-solid fa-bookmark"></i>}
     </span>
   );
 };
